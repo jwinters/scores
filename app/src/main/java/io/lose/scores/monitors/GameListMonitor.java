6d7c37accf70dae1666e1de738e2c4c98ef3a06c
Joshua Winters <winters.118@gmail.com>
2016-12-12 20:19:27 -0500

    Refresh games list when loaded

--- app/src/main/java/io/lose/scores/monitors/GameListMonitor.java
+++ app/src/main/java/io/lose/scores/monitors/GameListMonitor.java
@@ -2,28 +2,30 @@ package io.lose.scores.monitors;
 
 import android.content.Context;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import io.lose.scores.operations.GetEventsOperation;
 import io.pivotal.arca.dispatcher.Query;
 import io.pivotal.arca.dispatcher.QueryResult;
 import io.pivotal.arca.monitor.RequestMonitor.AbstractRequestMonitor;
 import io.pivotal.arca.service.Operation;
 import io.pivotal.arca.service.OperationService;
-import io.pivotal.arca.utils.ArrayUtils;
 
 public class GameListMonitor extends AbstractRequestMonitor {
 
-	@Override
-	public int onPostExecute(final Context context, final Query request, final QueryResult result) {
+    private Set<String> mSynced = new HashSet<>();
 
-        final boolean shouldSync = result.getData().getCount() == 0;
+    @Override
+    public int onPostExecute(final Context context, final Query request, final QueryResult result) {
 
-        if (shouldSync) {
-            final String[] args = request.getWhereArgs();
+        final String date = request.getWhereArgs()[0];
 
-            if (ArrayUtils.isNotEmpty(args)) {
-                final Operation operation = new GetEventsOperation(args[0]);
-                return OperationService.start(context, operation) ? Flags.DATA_SYNCING : 0;
-            }
+        if (!mSynced.contains(date)) {
+            mSynced.add(date);
+
+            final Operation operation = new GetEventsOperation(date);
+            return OperationService.start(context, operation) ? Flags.DATA_SYNCING : Flags.DATA_VALID;
         }
 
         return Flags.DATA_VALID;
